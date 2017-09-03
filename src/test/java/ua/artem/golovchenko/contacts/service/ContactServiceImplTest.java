318a48ff950943ef446bef4537fbf0a5d1fb98f1
Golovchenko Artem <golovchenkoart@gmail.com>
2017-09-03 18:43:06 +0300

    deleted blank row

--- src/test/java/ua/artem/golovchenko/contacts/service/ContactServiceImplTest.java
+++ src/test/java/ua/artem/golovchenko/contacts/service/ContactServiceImplTest.java
@@ -1,13 +1,11 @@
 package ua.artem.golovchenko.contacts.service;
 
-/*@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration( classes=Application.class)*/
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
+
         ContactRegExpTest.class,
         ContactServiceMainTest.class
 
